83af6d0e8a599becab30ac072afb18903ef2b589
Meash <m4rtinsch@gmail.com>
2015-09-24 09:43:36 +0200

    Fix icon retrieval

--- endpoints/RestApi_Multisites.php
+++ endpoints/RestApi_Multisites.php
@@ -34,8 +34,29 @@ class RestApi_Multisites extends RestApi_ExtensionBase {
 		return [
 			'id' => $blog['blog_id'],
 			'name' => $details->blogname,
+			'icon' => $this->get_icon($blog['blog_id']),
 			'path' => $blog['path'],
 			'description' => get_bloginfo($blog)
 		];
+	}
+
+	private function get_icon($blog_id) {
+		$posts = $blog_id > 1 ? "wp_${blog_id}_posts" : "wp_posts";
+		$postmeta = $blog_id > 1 ? "wp_${blog_id}_postmeta" : "wp_postmeta";
+		$query_string = "
+			SELECT post_content AS icon_path
+			FROM $posts
+			JOIN $postmeta ON $postmeta.post_id = $posts.ID
+			WHERE $postmeta.meta_key = '_wp_attachment_context'
+			  AND $postmeta.meta_value = 'site-icon'";
+		global $wpdb;
+		$results = $wpdb->get_results($query_string, OBJECT);
+		if (sizeof($results) > 1) {
+			throw new LogicException("More than one icon defined for site $blog_id");
+		}
+		if (sizeof($results) == 0) {
+			return null;
+		}
+		return $results[0]->icon_path;
 	}
 }
